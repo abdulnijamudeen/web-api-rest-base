21d206e43d0ae60a8ca5cffcf6945f05d5d2c5f0
Nizam-Shahul-Laptop <abdulnijamudeen@gmail.com>
2020-08-18 18:32:03 +0530

    Updated Service Layer User Service

---
+++ WebApiRestBase/ServiceLayer/Service/User/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Service.User
+{
+    interface IUserService
+    {
+        DataAccessLayer.EF.User GetUserByUsername(string username);
+    }
+}
---
+++ WebApiRestBase/ServiceLayer/Service/User/UserService.cs
@@ -0,0 +1,50 @@
+﻿using DataAccessLayer.Repository;
+using System;
+using System.Linq;
+
+namespace ServiceLayer.Service.User
+{
+    class UserService : IUserService, IDisposable
+    {
+        private UnitOfWork unitOfWork;
+        private bool disposedValue;
+
+        public UserService(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public DataAccessLayer.EF.User GetUserByUsername(string username) 
+            => unitOfWork.UserRepository.Get().AsQueryable().FirstOrDefault(u => u.Username.Equals(username));
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    unitOfWork.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~UserService()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
